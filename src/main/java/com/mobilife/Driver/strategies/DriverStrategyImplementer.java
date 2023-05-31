97d2dbaf87900e1fc4c08fc22792dc93da4701e1
Yakha12 <89409148+Yakha12@users.noreply.github.com>
2023-05-31 02:28:52 +0200

    Enhance Switch

--- src/main/java/com/mobilife/Driver/strategies/DriverStrategyImplementer.java
+++ src/main/java/com/mobilife/Driver/strategies/DriverStrategyImplementer.java
@@ -4,16 +4,12 @@ import com.mobilife.Utilities.Constants;
 
 public class DriverStrategyImplementer {
     public static DriverStrategy chooseStrategy(String strategy){
-        switch(strategy){
-            case Constants.CHROME:
-                return new Chrome();
-
-            case Constants.FIREFOX:
-                return new FireFox();
-
-            default:
-                return null;
-        }
+        //Enhanced Switch Statement
+        return switch (strategy) {
+            case Constants.CHROME -> new Chrome();
+            case Constants.FIREFOX -> new FireFox();
+            default -> null;
+        };
 
     }
 }
